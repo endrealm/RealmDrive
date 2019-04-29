92841c6fd78447829cf87b6992456f5c1bc02098
Gerolmed <werwolf420@yahoo.de>
2019-04-29 17:33:01 +0200

    Added reactive non blocking reader behaviour

--- src/main/java/net/endrealm/realmdrive/inst/SimpleDriveReader.java
+++ src/main/java/net/endrealm/realmdrive/inst/SimpleDriveReader.java
@@ -1,12 +1,15 @@
 package net.endrealm.realmdrive.inst;
 
+import net.endrealm.realmdrive.interfaces.DriveElement;
 import net.endrealm.realmdrive.interfaces.DriveObject;
 import net.endrealm.realmdrive.interfaces.DriveReader;
 import net.endrealm.realmdrive.interfaces.DriveService;
 import net.endrealm.realmdrive.query.Query;
+import net.endrealm.realmdrive.utils.ThreadUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * @author johannesjumpertz
@@ -33,6 +36,21 @@ public class SimpleDriveReader implements DriveReader {
     }
 
     /**
+     * Does the backend contain one or more objects that can be identified through the query details
+     *
+     * @param query    query details used to search for the objects
+     * @param onResult invoked upon result
+     */
+    @Override
+    public void containsObjectAsync(Query query, Consumer<Boolean> onResult) {
+        ThreadUtils.createNewThread(
+                () -> {
+                    onResult.accept(containsObject(query));
+                }
+        );
+    }
+
+    /**
      * Read the first object instance found by the specified parameters
      *
      * @param query query details used to search for the objects
@@ -41,6 +59,26 @@ public class SimpleDriveReader implements DriveReader {
     @Override
     public DriveObject readObject(Query query) {
         return driveService.getBackend().findOne(query);
+    }
+
+    /**
+     * Read the first object instance found by the specified parameters
+     *
+     * @param query     query details used to search for the objects
+     * @param onSuccess this is invoked if a result is received
+     * @param onFailure this is invoked if no result is found
+     */
+    @Override
+    public void readObjectAsync(Query query, Consumer<DriveObject> onSuccess, Runnable onFailure) {
+        ThreadUtils.createNewThread(
+                () -> {
+                    DriveObject result = readObject(query);
+                    if(result != null)
+                        onSuccess.accept(result);
+                    else
+                        onFailure.run();
+                }
+        );
     }
 
     /**
@@ -57,6 +95,34 @@ public class SimpleDriveReader implements DriveReader {
     }
 
     /**
+     * Read the first object instance found by the specified parameters
+     *
+     * @param query     query details used to search for the objects
+     * @param clazz     clazz to convert the result into
+     * @param onSuccess invoked upon found result
+     * @param onFailure invoked when no result is queried
+     * @param onError   called if an error occurs
+     */
+    @Override
+    public <T> void readObjectAsync(Query query, Class<T> clazz, Consumer<T> onSuccess, Runnable onFailure, Consumer<Throwable> onError) {
+        ThreadUtils.createNewThread(
+                () -> {
+                    try {
+                        T result = readObject(query, clazz);
+
+                        if(result != null)
+                            onSuccess.accept(result);
+                        else
+                            onFailure.run();
+
+                    } catch (Exception ex) {
+                        onError.accept(ex);
+                    }
+                }
+        );
+    }
+
+    /**
      * Read all object instances found by the specified parameters
      *
      * @param query query details used to search for the objects
@@ -65,6 +131,22 @@ public class SimpleDriveReader implements DriveReader {
     @Override
     public Iterable<DriveObject> readAllObjects(Query query) {
         return driveService.getBackend().findAll(query);
+    }
+
+    /**
+     * Read all object instances found by the specified parameters
+     *
+     * @param query    query details used to search for the objects
+     * @param onResult invoked upon result
+     */
+    @Override
+    public void readAllObjectsAsync(Query query, Consumer<Iterable<DriveObject>> onResult) {
+        ThreadUtils.createNewThread(
+                () -> {
+                    Iterable<DriveObject> result = readAllObjects(query);
+                    onResult.accept(result);
+                }
+        );
     }
 
     /**
@@ -82,6 +164,29 @@ public class SimpleDriveReader implements DriveReader {
             list.add(driveService.getConversionHandler().transform(obj, clazz))
 ;        }
         return list;
+    }
+
+    /**
+     * Read all object instances found by the specified parameters
+     *
+     * @param query     query details used to search for the objects
+     * @param clazz     clazz to convert the results into
+     * @param onSuccess invoked upon result
+     * @param onError   called when an error occurs
+     */
+    @Override
+    public <T> void readAllObjects(Query query, Class<T> clazz, Consumer<Iterable<T>> onSuccess, Consumer<Throwable> onError) {
+        ThreadUtils.createNewThread(
+                () -> {
+                    try {
+                        Iterable<T> result = readAllObjects(query, clazz);
+                        onSuccess.accept(result);
+
+                    } catch (Exception ex) {
+                        onError.accept(ex);
+                    }
+                }
+        );
     }
 
     /**
--- src/main/java/net/endrealm/realmdrive/interfaces/DriveReader.java
+++ src/main/java/net/endrealm/realmdrive/interfaces/DriveReader.java
@@ -2,6 +2,8 @@ package net.endrealm.realmdrive.interfaces;
 
 import net.endrealm.realmdrive.query.Query;
 
+import java.util.function.Consumer;
+
 /**
  * @author johannesjumpertz
  *
@@ -11,10 +13,19 @@ public interface DriveReader {
 
     /**
      * Does the backend contain one or more objects that can be identified through the query details
+     *
      * @param query query details used to search for the objects
      * @return one or more instances found
      */
     boolean containsObject(Query query);
+
+    /**
+     * Does the backend contain one or more objects that can be identified through the query details
+     *
+     * @param query query details used to search for the objects
+     * @param onResult invoked upon result
+     */
+    void containsObjectAsync(Query query, Consumer<Boolean> onResult);
 
     /**
      * Read the first object instance found by the specified parameters
@@ -26,6 +37,15 @@ public interface DriveReader {
     /**
      * Read the first object instance found by the specified parameters
      * @param query query details used to search for the objects
+     * @param onSuccess this is invoked if a result is received
+     * @param onFailure this is invoked if no result is found
+     */
+    void readObjectAsync(Query query, Consumer<DriveObject> onSuccess, Runnable onFailure);
+
+    /**
+     * Read the first object instance found by the specified parameters
+     *
+     * @param query query details used to search for the objects
      * @param clazz clazz to convert the result into
      * @param <T> will try to convert the object into the type
      * @return first object that was found. Returns null if none is found
@@ -34,11 +54,33 @@ public interface DriveReader {
     <T> T readObject(Query query, Class<T> clazz) throws ClassCastException;
 
     /**
+     * Read the first object instance found by the specified parameters
+     *
+     * @param query query details used to search for the objects
+     * @param clazz clazz to convert the result into
+     * @param onSuccess invoked upon found result
+     * @param onFailure invoked when no result is queried
+     * @param onError called if an error occurs
+     * @param <T> will try to convert the object into the type
+     */
+    <T> void readObjectAsync(Query query, Class<T> clazz, Consumer<T> onSuccess, Runnable onFailure, Consumer<Throwable> onError);
+
+    /**
      * Read all object instances found by the specified parameters
+     *
      * @param query query details used to search for the objects
      * @return objects that were found
      */
     Iterable<DriveObject> readAllObjects(Query query);
+
+
+    /**
+     * Read all object instances found by the specified parameters
+     *
+     * @param query query details used to search for the objects
+     * @param onResult invoked upon result
+     */
+    void readAllObjectsAsync(Query query, Consumer<Iterable<DriveObject>> onResult);
 
     /**
      * Read all object instances found by the specified parameters
@@ -49,6 +91,17 @@ public interface DriveReader {
      * @throws ClassCastException thrown if found objects can not be converted into T
      */
     <T> Iterable<T> readAllObjects(Query query, Class<T> clazz) throws ClassCastException;
+
+
+    /**
+     * Read all object instances found by the specified parameters
+     * @param query query details used to search for the objects
+     * @param clazz clazz to convert the results into
+     * @param <T> will try to convert the objects into the type
+     * @param onSuccess invoked upon result
+     * @param onError called when an error occurs
+     */
+    <T> void readAllObjects(Query query, Class<T> clazz, Consumer<Iterable<T>> onSuccess, Consumer<Throwable> onError);
 
     /**
      * Sets the service the reader is used by.
